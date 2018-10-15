cd8a0f805d5a4f8a92de2ab13550d7019d4099f0
Austin.Woo <ctwyb@163.com>
2018-10-15 09:41:50 +0800

    add resource

---
+++ export/adm_raw_export_h1_lstm_v1.sql
@@ -0,0 +1,28 @@
+drop table if exists export.adm_raw_export_h1_lstm_v1;
+select
+--  top 100
+  cast(convert(varchar(10),a.pricedate,120)+' '+convert(varchar(8),a.pricetime,108) as datetime) as pricedatetime,
+  [open],
+  high,
+  low,
+  [close],
+  volume,
+  macd_main,
+  macd_signal,
+  rsi,
+  kdj_main,
+  kdj_signal,
+  ma_18,
+  ma_36,
+  ma_56,
+  price_2avg,
+  lable_h1_price_2avg
+into export.adm_raw_export_h1_lstm_v1
+from ods.rawdata_h1 a
+    inner join adm.h1_lable b
+      on a.currency=b.currency and
+         a.pricedate=b.pricedate and
+         a.pricetime=b.pricetime
+where   year(a.pricedate) in (2017,2018) and lable_h1_price_2avg is not null
+order by a.pricedate,a.pricetime
+;
